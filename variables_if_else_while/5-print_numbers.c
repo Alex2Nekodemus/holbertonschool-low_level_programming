ea179188ba7b91387cf392fad4c858661f4ad13b
Alex2Nekodemus <nekodemus@oblat.eu>
2024-06-18 10:53:45 +0200

    Release task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+ * print single digit numbers of base 10
+*/
+
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int x;
+for (x = 0; x <= 9; x++)
+	putchar(x);
+putchar('\n');
+return (0);
+}
