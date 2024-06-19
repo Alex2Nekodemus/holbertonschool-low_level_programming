f33084df77faa9cc2de281e56a7c25e965e0a450
Alex2Nekodemus <nekodemus@oblat.eu>
2024-06-19 14:12:35 +0200

    Modify description to match betty

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,26 +1,19 @@
 #include "main.h"
 
 /**
- * print the last digit of a number
+ * print_last_digit - Give the last digit of a number
+ * @c: The number to find the last digit from
+ *
+ * Return: y as the last digit
  */
-
-/**
- * print_last_digit -  give the last digit
- * @c - the number to find the last digit from
- * @y - the last digit
- * Return - y as the last digit
- */
-
 int print_last_digit(int c)
 {
-int y;
+	int y;
 
-y = c % 10;
+	y = (c % 10);
+	if (y < 0)
+		y = (-1 * y);
 
-if (y < 0)
-	y = -1 * y;
-else
 	_putchar('0' + y);
-
-return (y);
+	return (y);
 }
