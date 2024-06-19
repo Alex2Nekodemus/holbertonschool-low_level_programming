58496db9cf3054c4d5ab51e4a0049a202832a70d
Alex2Nekodemus <nekodemus@oblat.eu>
2024-06-19 09:14:37 +0200

    Put .c file not compilation

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * compute the absolute value of an integer
+ */
+
+/**
+ * _abs - function to compute the absolute value
+ * @arg1 - int to dertermine the absolute value of
+ * Return - c variable as absolute
+ */
+
+int _abs(int c)
+{
+
+if (c < 0)
+	c = -1 * c;
+
+return (c);
+}
