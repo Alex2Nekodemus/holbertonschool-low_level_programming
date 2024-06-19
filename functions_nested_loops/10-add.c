2c53b2d57f458cb9e64991a262a8c08a44550141
Alex2Nekodemus <nekodemus@oblat.eu>
2024-06-19 11:16:08 +0200

    Release task 10 with same error to describe function

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/*
+ * add - function adds to integers and return the result
+ * @a - first integer
+ * @b -second integer
+ * return - c the result of this add
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = (a + b);
+	return (c);
+}
